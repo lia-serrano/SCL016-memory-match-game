b8c510e4d7c44c60a2218ec32099db4adabd0949
lia-serrano <liaserranolopez@gmail.com>
2021-03-30 21:35:27 -0300

    paginas listas en html

---
+++ src/data/Rick and Morty/rick.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'rickAndMorty',
+    items: [
+      { id: 'meeseeks', image: '' , bgColor: 'D9C3EE' }, 
+      { id: 'scary', image: '../image/scary.png', bgColor: 'D9C3EE' },
+      { id: 'spaceship', image: '', bgColor: 'D9C3EE' },
+      { id: 'vortex', image: '', bgColor: 'D9C3EE' },
+
+    ],
+  };
+  
